b52b1fb95beceee7a54726fb5c92b2b8478d3da6
Houser164 <110843443+Houser164@users.noreply.github.com>
2023-10-09 22:04:31 +0700

    midterm

---
+++ assignment7/week5/assignment5/main.js
@@ -0,0 +1,3 @@
+document.getElementById("pID").innerText = ("146"+52);
+
+document.getElementById("KID").innerText = (22+0);
---
+++ assignment7/week7/Q2/main.js
@@ -0,0 +1,6 @@
+
+
+
+window.onload = function(e){ 
+    document.getElementById("hID").innerText = (122+154);
+}
